c9773738739707c23b61992046a2a4f9f22ce4ed
Arvind <ranaarvind156@gmail.com>
2023-10-09 14:10:14 +0530

    Two Best Non-Overlapping Events

---
+++ 037. Two Best Non-Overlapping Events/BruteForce.py
@@ -0,0 +1,20 @@
+def maxTwoEvents(events) -> int:
+    maxSum = 0
+    
+    # For each event
+    for i in range(len(events)): 
+        sum = events[i][2]
+        
+        # Try to find the event that can be paired with it to get a maximum sum
+        for j in range(len(events)):
+            if i != j and events[j][0] > events[i][1]:
+                maxSum = max(maxSum, sum + events[j][2])            
+        
+        # This is for the cases when we cannot find any other event with which this event can be paired
+        # It is also possible that a single event has a higher value than any other valid pair combined
+        maxSum = max(maxSum, sum)
+    return maxSum
+
+events = [[1,5,3],[1,5,1],[6,6,5]]
+
+print("Output -> ", maxTwoEvents(events))
